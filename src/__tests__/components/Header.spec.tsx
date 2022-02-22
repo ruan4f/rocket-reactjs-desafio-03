3d722e418852d9dff5912f056271e77bb180c484
Ruan Ferreira <8647151+ruan4f@users.noreply.github.com>
2022-02-21 21:14:22 -0300

    Cart Size

--- src/__tests__/components/Header.spec.tsx
+++ src/__tests__/components/Header.spec.tsx
@@ -15,16 +15,14 @@ jest.mock('../../hooks/useCart', () => {
         {
           amount: 2,
           id: 1,
-          image:
-            'https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/modulo-redux/tenis1.jpg',
+          image: 'https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/modulo-redux/tenis1.jpg',
           price: 179.9,
           title: 'Tênis de Caminhada Leve Confortável',
         },
         {
           amount: 1,
           id: 2,
-          image:
-            'https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/modulo-redux/tenis2.jpg',
+          image: 'https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/modulo-redux/tenis2.jpg',
           price: 139.9,
           title: 'Tênis VR Caminhada Confortável Detalhes Couro Masculino',
         },
